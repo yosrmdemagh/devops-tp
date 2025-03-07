9bd481d0373b3d53ff8033d3a45adba219202b57
Yosr Mdemagh <yosrmdemagh@gmail.com>
2025-03-07 23:28:59 +0100

    Ajout de la route /status et de son test

--- test_app.py
+++ test_app.py
@@ -3,6 +3,15 @@ from app import app
 
 class TestApp(unittest.TestCase):
     def test_home_route(self):
-        tester = app.test_client()
+        tester = app.test_client(self)
         response = tester.get('/')
         self.assertEqual(response.status_code, 200)
+
+    def test_status_route(self):
+        tester = app.test_client(self)
+        response = tester.get('/status')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {"status": "running"})
+
+if __name__ == "__main__":
+    unittest.main()
